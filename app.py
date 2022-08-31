fcdc7fe0ce6132362ea528ff2fc443ed984b4ce2
pinetree100 <111733786+pinetree100@users.noreply.github.com>
2022-08-31 16:37:07 +0100

    Update app.py
    Bug fixes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 #from db import db
@@ -8,6 +10,9 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = 'pass'
@@ -20,6 +25,14 @@ api.add_resource(StoreList, '/stores')
 
 #api.add_resource(UserRegister, '/register')
 #to protect from import impositions
+
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+            
+    app.run(port=5000)
